ab0675ee587160014100ee635376074f9547205b
NOAH0883 <56993972+NOAH0883@users.noreply.github.com>
2026-07-23 12:39:20 +1200

    camQuotes

--- Assets/Me Me ME/CameraMove.cs
+++ Assets/Me Me ME/CameraMove.cs
@@ -1,14 +1,21 @@
+using System;
+using TMPro;
 using Unity.Cinemachine;
 using Unity.Hierarchy;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CameraMove : MonoBehaviour
 {
 
     [SerializeField] CinemachineCamera[] cameras;
     int cameraIndex;
-    
-    
+
+    [SerializeField] string[] fishQuotes;
+    public int randomNumber;
+    private float floorsFallen;
+    [SerializeField] TextMeshProUGUI quotesDisplay;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,25 +23,40 @@ public class CameraMove : MonoBehaviour
         
         cameraIndex = 0;
         cameras[cameraIndex].Priority = 1;
+        Debug.Log(fishQuotes.Length);
+    }
+
+    private void Update()
+    {
+        quotesDisplay.text = fishQuotes[randomNumber];
     }
 
     public void MoveCamUp()
     {
         
-            cameraIndex++;
-            cameras[cameraIndex].Priority = 1;
-            cameras[cameraIndex - 1].Priority = 1;
+        cameraIndex++;
+        cameras[cameraIndex].Priority = 1;
+        cameras[cameraIndex - 1].Priority = 1;
         
     }
 
     public void MoveCamDown()
     {
-   
             cameraIndex--;
             cameras[cameraIndex].Priority = 1;
             cameras[cameraIndex + 1].Priority = 0;
   
 
+        if(floorsFallen == 2)
+        {   
+            DisplayQuote();
+            floorsFallen = 0;
+            
+        }
+        else
+        {
+            floorsFallen++;
+        }
 
         //falling quotes
 
@@ -52,8 +74,15 @@ public class CameraMove : MonoBehaviour
         //if fallenFoors is equal to or greater than fallenFloorsMax
         //get a  random number from 0 to amount of quotes
         
-        //display the random quote with canvas and textMeshPro - for as long as timer
+        //display the random quote with canvas and textMeshPro 
+        //text on - text off baised of timer
         //reset fallenFloors
+    }
 
+    private void DisplayQuote()
+    {
+        Debug.Log("Quote...");
+        randomNumber = Random.Range(0, fishQuotes.Length-1);
+          
     }
 }
